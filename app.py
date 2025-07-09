5dd20410176a7f82169e620b2539d17c1280929e
Cser Péter <cserpeti011@gmail.com>
2025-07-09 09:36:29 +0200

    Fix: Flask app futtatása Render-hez, port és host beállítás

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ def index():
 
     return render_template('index.html', BASE_URLS=BASE_URLS)
 
+import os
+
 if __name__ == '__main__':
-    webbrowser.open("http://127.0.0.1:5000")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
